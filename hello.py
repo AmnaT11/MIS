94095866de43dd558ab3363909155279c946ee67
AmnaT11 <atariq2@babson.edu>
2016-09-08 17:29:26 -0400

    Session 2

--- hello.py
+++ hello.py
@@ -1,1 +1,15 @@
-print("Hello, world!")
+#print("Hello, world!")
+#print("Hey Jude, Don't make it bad")
+#print("46+37+38=",46+37+38)
+#message = 'Please enter your name: '
+#name = input(message)
+#print('hello, ',name)
+
+last_name="Tariq"
+first_name="amna"
+name=first_name+' '+last_name
+
+message = 'hello, '+name+'\n'
+print(message * 10)
+
+input()
