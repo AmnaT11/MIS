5bf364a794da33c0b26f853696336ac4153ee0d8
AmnaT11 <atariq2@babson.edu>
2016-09-19 22:07:37 -0400

    HW#3

---
+++ mypolygon.py
@@ -0,0 +1,21 @@
+import turtle
+amna = turtle.Turtle()
+
+print(amna)
+
+amna.fd(90)
+amna.lt(90)
+amna.fd(90)
+amna.lt(90)
+amna.fd(90)
+amna.lt(90)
+amna.fd(90)
+
+print(amna)
+
+#turtle.mainloop()
+
+jerry=turtle.Turtle()
+
+polygon(jerry)
+
