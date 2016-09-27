6ba761aef2fa773d7da64d69d998fe7fe775fa3f
AmnaT11 <atariq2@babson.edu>
2016-09-27 13:35:41 -0400

    Quiz Q-3

---
+++ Quiz.py
@@ -0,0 +1,4 @@
+def sum_squares(n):
+    return sum([i**2 for i in range(1, n+1)])
+print(sum_squares(1))
+print(sum_squares(100))
---
+++ Sumusingforloop.py
@@ -0,0 +1,8 @@
+result = 0
+for i in range(11):
+    result += i
+print(result)
+print(1+2+3+4+5+6+7+8+9+10)
+
+#result += i means that result = result + i
+
